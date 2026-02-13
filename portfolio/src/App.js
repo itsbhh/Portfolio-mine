dab55b698a09babea3f302a15b38724a83b71fff
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2026-02-13 11:22:25 +0530

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,6 +6,7 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Footer } from "./components/Footer";
+import { Contact } from './components/Contact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
       <Footer />
     </div>
   );
