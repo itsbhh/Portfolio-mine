11ffa3c57ebd7c0b9ec6cb822c10f0816f30b35d
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2026-07-26 11:21:02 +0530

    Create Projects.js

---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,33 @@
+import { Container, Row, Col, Tab, Nav, Button, Modal } from "react-bootstrap";
+import { useState } from "react";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import skynetImg from "../assets/img/skynet.png";
+import bookstoreImg from "../assets/img/bookstore.png";
+import voiceAssistantImg from "../assets/img/voice-assistant.jpeg";
+import zoomCloneImg from "../assets/img/zoom-clone.jpeg";
+import websiteDesignImg from "../assets/img/website-design.jpg";
+import weatherApiImg from "../assets/img/weather-api.png";
+import imageApiImg from "../assets/img/image-api.png";
+import newsApiImg from "../assets/img/news-api.png";
+import codsoftImg from "../assets/img/codsoft.png";
+import gifApiImg from "../assets/img/gif-api.png";
+import recipeApiImg from "../assets/img/recipe-api.png";
+import coffeeShopImg from "../assets/img/coffee-shop.png";
+import movieGuideImg from "../assets/img/movie-guide.png";
+import todoFlutterImg from "../assets/img/todo-flutter.jpeg";
+import todoImg from "../assets/img/todo.png";
+import bloodBankImg from "../assets/img/blood-bank.png";
+import MerryImg from "../assets/img/merry.png";
+import ZerodhaImg from "../assets/img/zerodha.png";
+import lovegramImg from "../assets/img/lovegram.png";
+import plagiarismCheckerImg from "../assets/img/plagiarism-checker.jpg";
+import languageTranslationImg from "../assets/img/language-translation.jpg";
+import facialEmotionRecognitionImg from "../assets/img/facial-emotion-recognition.jpg";
+import donationdappImg from "../assets/img/donation-dapp.png";
+import eduwizardImg from "../assets/img/eduwizard.jpg";
+import jobhubImg from "../assets/img/job-hub.png";
+import aviaai from "../assets/img/AviaAI.png";
+
+
