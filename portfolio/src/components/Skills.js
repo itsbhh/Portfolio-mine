322a6cb03bef1c75c1bb03c7ad694fca728a0bbd
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2026-08-02 11:22:41 +0530

    Update Skills.js

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -25,4 +25,34 @@ export const Skills = () => {
     },
   };
 
+  const skills = [
+    { name: "Product Thinking", img: meter3 },
+    { name: "User Research", img: meter2 },
+    { name: "MERN Stack", img: meter3 },
+    { name: "React.js", img: meter3 },
+    { name: "Node.js", img: meter2 },
+    { name: "Wireframing (Figma)", img: meter2 },
+    { name: "User Journey Mapping", img: meter3 },
+    { name: "Agile & Scrum", img: meter1 },
+    { name: "Problem Solving", img: meter3 },
+    { name: "Jira", img: meter1 },
+    { name: "Notion", img: meter2 },
+    { name: "Google Analytics", img: meter1 },
+    { name: "Figma", img: meter3 },
+    { name: "Feature Prioritization", img: meter1 },
+    { name: "Roadmapping", img: meter2 },
+    { name: "A/B Testing", img: meter3 },
+    { name: "Data Analysis", img: meter1 },
+    { name: "Java", img: meter1 },
+    { name: "Python", img: meter2 },
+    { name: "JavaScript", img: meter1 },
+    { name: "MongoDB", img: meter1 },
+    { name: "MySQL", img: meter1 },
+    { name: "Docker", img: meter2 },
+    { name: "Kubernetes", img: meter1 },
+    { name: "Cassandra", img: meter1 },
+    { name: "Machine Learning", img: meter3 },
+    { name: "AI & ML", img: meter3 },
+  ];
+
  
